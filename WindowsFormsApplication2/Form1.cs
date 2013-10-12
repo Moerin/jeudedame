bda6ac6159a284fb0e8428188beef135b511c0ee
Moerin <trash.sebatien@gmail.com>
2013-10-13 00:26:49 +0200

    Commentaire sur le code

--- WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/Form1.cs
@@ -12,28 +12,44 @@ namespace WindowsFormsApplication2
 {
     public partial class Form1 : Form
     {
+        // Buffer pour le double buffering et eviter le flickering a chaque evenement
         private Bitmap _backBuffer;
+
+        // Taille d'une case
         int square_w = 50;
+        // Taille d'un pion
         int pion_w = 40;
+        // Coordonnees
         int cursor_x, cursor_y;
 
+        // Listes de pions pour les garder
         List<Pion> liste_pion = new List<Pion>();
 
+        // La fenetre principale
         public Form1()
         {
+            // Double buffering
             SetStyle(ControlStyles.OptimizedDoubleBuffer, true); 
             SetStyle(ControlStyles.DoubleBuffer, true);
             InitializeComponent();
+
+            // ???
             cursor_x = -1;
             cursor_y = -1;
+
+            // Appel de la focntion creations de pions
             create_pions();
         }
 
+        // Dessine les elements sur le plateau de jeu
         protected override void OnPaint(PaintEventArgs e)
         {
+            // Singleton du buffer pour le double buffering
             if (_backBuffer == null) {
                 _backBuffer = new Bitmap(this.ClientSize.Width, this.ClientSize.Height);
             }
+
+            // Objet graphique pour dessiner l'image
             Graphics g = Graphics.FromImage(_backBuffer);
             // Clear window
             Brush brush = new SolidBrush(Color.White);
@@ -46,9 +62,12 @@ namespace WindowsFormsApplication2
             //base.OnPaint (e); //optional but not recommended
         }
 
+        // Gere les evenement en lien avec les cliques de la souris
         protected override void OnClick(EventArgs e)
         {
             //base.OnClick(e);
+            
+            // Boit de message qui affiche les coordonnes du curseur
             MessageBox.Show("abscisse Curseur : " + cursor_x + ", " + "ordonnee Curseur : " + cursor_y);
 
         }
@@ -58,55 +77,82 @@ namespace WindowsFormsApplication2
             //Don't allow the background to paint
         }
 
+        // Affiche tous les elements qui doivent etre present sur la fenetre
         void liste_affichage(Graphics g)
         {
+            // Dessine les cases
             draw_grid(g);
+
+            // Affiche les coordonnees du curseur dans la boite de dialogue
             draw_cursor();
+
+            // Dessine les pions sur le damier
             draw_pions(g);
         }
 
+        // Creation des pions
         void create_pions()
         {
+            // Boucle représentant le parcours des cases en colonne
             for (int y = 0; y < 4; y++)
             {
+                // Boucle représentant le parcours des cases en ligne
                 for (int x = 0; x < 5; x++)
                 {
+                    // Test si les lignes du damier sont paires et si c'est le cas les pions seront décalés d'une case
                     if ((y % 2) == 0)
                     {
+                        // Ajout des pions noirs
                         liste_pion.Add(new Pion(2 * x + 1, y, pion_w, Pion.pion_gender.Black));
                     }
+                    // Sinon ils ne seront pas décalés
                     else {
+                        // Ajout des pions noirs
                         liste_pion.Add(new Pion(2 * x, y, pion_w, Pion.pion_gender.Black));
                     }
                 }
             }
 
+            // Boucle représentant le parcours des cases en colonne
             for (int y = 6; y < 10; y++)
             {
+
+                // Boucle représentant le parcours des cases en ligne
                 for (int x = 0; x < 5; x++)
                 {
+                    // Test si les lignes du damier sont paires et si c'est le cas les pions seront décalés d'une case
                     if ((y % 2) == 0)
                     {
+                        // Ajout des pions blancs
                         liste_pion.Add(new Pion(2 * x + 1, y, pion_w, Pion.pion_gender.White));
                     }
+                    // Sinon ils ne seront pas décalés
                     else
                     {
+                        // Ajout des pions blancs
                         liste_pion.Add(new Pion(2 * x, y, pion_w, Pion.pion_gender.White));
                     }
                 }
             } 
         }
 
-        // quadrillage de la surface de jeu
+        // Quadrillage de la surface de jeu
         void draw_grid(Graphics g)
         {
+            // ???
             Brush brush = new SolidBrush(Color.Black);
+
+            // Boucle qui parcours le damier en colonne
             for (int iy = 0; iy < 10; iy++)
             {
+                // Boucle qui parcours le damier en ligne
                 for (int ix = 0; ix < 5; ix++)
                 {
+                    // Ajout des case noires
                     int x = square_w * 2 * ix + square_w;
                     int y = iy * square_w;
+                    
+                    // si le chiffre de la colonne est pair il y a decalage
                     if (iy % 2 != 0)
                     {
                         g.FillRectangle(brush, x - square_w, y, square_w, square_w);
@@ -117,10 +163,13 @@ namespace WindowsFormsApplication2
                     }
                 }
             }
+
+            // ???
             Pen pen = new Pen(Color.Black);
             g.DrawRectangle(pen, 0, 0, square_w * 10, square_w * 10);
         }
 
+        // Dessin des pions
         void draw_pions(Graphics g)
         {  
             foreach (Pion p in liste_pion)
@@ -129,12 +178,14 @@ namespace WindowsFormsApplication2
             }
         }
 
+        // Affichage les coordonnées du curseur dans la boite de dialogue
         void draw_cursor()
         {
             Console.WriteLine("X: " + cursor_x);
             Console.WriteLine("Y: " + cursor_y);
         }
 
+        // Methode qui recupere les coordonnée du curseur
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
             Invalidate();
--- WindowsFormsApplication2/Pion.cs
+++ WindowsFormsApplication2/Pion.cs
@@ -7,16 +7,25 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApplication2
 {
+    /// <summary>
+    /// Classe representant les pions du jeu
+    /// </summary>
     class Pion
     {
+        // Enumeration representant la couleur noire et blanche
         public enum pion_gender { White, Black };
 
+        // Coordonnées pion
         private int x;
         private int y;
+
+        // Taille pion
         private int size;
+
+        // Couleur du pion
         private pion_gender gender;
 
-
+        // Constructeur
         public Pion(int x, int y, int size, pion_gender gender) 
         {
             this.x = x;
@@ -25,9 +34,12 @@ namespace WindowsFormsApplication2
             this.gender = gender;
         }
 
+        // Dessin du pion
         public void draw(Graphics g, int square_w)
         {
             Brush brush;
+
+            // Test couleur du pion
             if (gender == pion_gender.White)
             {
                 brush = new SolidBrush(Color.Beige);
@@ -37,10 +49,13 @@ namespace WindowsFormsApplication2
             }
             Pen pen = new Pen(Color.Snow);
 
+            // Conversion coordonnées en entier [0-9] --- Centrage du pion dans la case
             int x_px = x * square_w + (square_w - size) / 2;
             int y_px = y * square_w + (square_w - size) / 2;
+            // Taille du pion
             int w_px = size;
             int h_px = size;
+            // Remplissage couleur
             g.FillEllipse(brush, x_px, y_px, w_px, h_px);
             g.DrawEllipse(pen, x_px, y_px, w_px, h_px);
         }
